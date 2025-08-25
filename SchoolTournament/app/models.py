8ed9dd894cabe29308f2f88bd242df01aafea8d4
Beyzanur <beyzanurdincer502@gmail.com>
2025-08-25 22:04:35 +0300

    SchoolTournament is created

--- SchoolTournament/app/models.py
+++ SchoolTournament/app/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 class Student(models.Model):
-    name=models.CharField(max_length=50),
-    student_number=models.IntegerField(),
+    name=models.CharField(max_length=50)
+    student_number=models.IntegerField()
     grade=models.IntegerField()
     
     def __str__(self):
         return self.name
     
 class Match(models.Model):
-    team_a=models.CharField(max_length=50),
-    team_b=models.CharField(max_length=50),
-    date=models.DateField(),
+    team_a=models.CharField(max_length=50)
+    team_b=models.CharField(max_length=50)
+    date=models.DateField()
     score_a=models.IntegerField()
     score_b=models.IntegerField()
     
@@ -19,7 +19,7 @@ class Match(models.Model):
         return self.date
 
 class Team(models.Model):
-    name=models.CharField(max_length=50),
+    name=models.CharField(max_length=50)
     coach=models.CharField(max_length=50)
     members=models.CharField(max_length=50)
     
--- SchoolTournament/app/urls.py
+++ SchoolTournament/app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("",views.add_student,name="add_student"),
     path("delete_student/<int:pk>/",views.delete_student,name="delete_student"),
     path("update_student/<int:pk>/",views.update_student,name="update_student"),
-    path("all_student",views.all_students,name="all_student"),
+    path("all_student",views.all_student,name="all_student"),
     
     path("",views.add_team,name="add_team"),
     path("delete_team/<int:pk>/",views.delete_team,name="delete_team"),
